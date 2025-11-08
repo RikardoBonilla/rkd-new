8a80604e7a956439483a309ad6fd7c441d6f8e8c
Ricardo <ricardoandresbonilla@gmail.com>
2025-11-08 00:35:26 -0500

    Initial commit: rkd-news - Ionic Angular MVVM

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.scientificnewshub.com',
+  apiTimeout: 30000,
+  cacheExpiration: 3600000,
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:3000/api',
+  apiTimeout: 30000,
+  cacheExpiration: 3600000,
+}
